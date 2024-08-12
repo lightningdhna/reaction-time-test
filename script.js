119513159557ecb7e54cd5ee07adb68d003bf399
lidh184z@gmail.com <lidh184z@gmail.com>
2024-08-12 12:08:31 +0700

    update-1

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const reactionTimeDisplay = document.getElementById('reaction-time');
 const averageTimeDisplay = document.getElementById('average-time');
 
 function getRandomTime() {
-    return Math.floor(Math.random() * 5000) + 2000; // Random time between 2 to 7 seconds
+    return Math.floor(Math.random() * 6000) + 1000; // Random time between 2 to 7 seconds
 }
 
 function startTest() {
     reactionTest.style.backgroundColor = 'red';
     message.textContent = 'Wait for green...';
-    reactionTimeDisplay.textContent = '';
+    // reactionTimeDisplay.textContent = '';
     setTimeout(() => {
         reactionTest.style.backgroundColor = 'green';
         message.textContent = 'Click!';
